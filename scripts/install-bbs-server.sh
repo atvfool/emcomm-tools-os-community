6b3744fa2efc1ce1379f54ebb9793df3a87783ff
Gaston Gonzalez <info@thetechprepper.com>
2025-03-26 06:35:00 -0700

    et-bbs-server - Added web interface for Linbpq to installer

--- scripts/install-bbs-server.sh
+++ scripts/install-bbs-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author  : Gaston Gonzalez
 # Date    : 8 November 2024
-# Updated : 28 November 2024
+# Updated : 26 March 2025
 # Purpose : Install Linbpq
 #
 # Notes:
@@ -19,6 +19,36 @@ apt install ucspi-tcp -y
 
 et-log "Installing Linbpq..."
 
+# Install the web interface for LinBPQ
+HTML_ZIP_FILE=HTMLPages.zip
+HTML_ZIP_URL="http://www.cantab.net/users/john.wiseman/Downloads/Beta/${HTML_ZIP_FILE}"
+
+curl -s -f -L -o ${HTML_ZIP_FILE} ${HTML_ZIP_URL}
+if [[ $? -eq 0 ]]; then
+   et-log "Downloading web interface for LinBPQ from ${HTML_ZIP_URL}"
+
+   CWD_DIR=$(pwd)
+
+   et-log "Installing web interface under ${BBS_HOME}"
+   BBS_HOME="/etc/skel/.local/share/emcomm-tools/bbs-server"
+   BBS_HOME_FILES="${BBS_HOME}/Files"
+
+   [[ ! -e ${BBS_HOME_FILES} ]] && mkdir -p ${BBS_HOME_FILES}
+
+   mv ${HTML_ZIP_FILE} ${BBS_HOME} && cd ${BBS_HOME} && unzip ${HTML_ZIP_FILE}
+
+   cd ${CWD_DIR}
+
+   if [[ -e "${BBS_HOME}/index.htmlx" ]]; then
+     et-log "Web interface installation successful"
+   else
+     et-log "Web interface installation failed"
+   fi
+else
+   et-log "Unable install web interface for LinBPQ"
+fi
+
+# Install Linbpq
 et-log "Installing 32-bit dependencies for Linbpq..."
 dpkg --add-architecture i386 &&  apt update && apt install \
   libpcap0.8-dev:i386 \
@@ -45,3 +75,4 @@ ln -s ${INSTALL_DIR} ${LINK_PATH}
 stow -v -d /opt linbpq -t /usr/local
 
 cd ${CWD_DIR}
+
